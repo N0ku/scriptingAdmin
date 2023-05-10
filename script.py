7179b2fbf0132eeaa6d7fb911ceb1e254dc0271f
N0ku <lucielecouturier2@gmail.com>
2023-05-10 16:44:12 +0200

    update

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 
 bucket = "test"
 
-client = InfluxDBClient(url="http://localhost:8086", token="", org="DevTeam")
+client = InfluxDBClient(url="http://localhost:8086", token="746Ify-d_159R7Hm_SVNLsok-UGdd3iMqOg9nkGmS4YBaAMNPllhI24gEYeCkbRXJhLBKxlBt0B7lzuBn-qvJQ==", org="DevTeam")
 
 write_api = client.write_api(write_options=SYNCHRONOUS)
 query_api = client.query_api()
@@ -13,7 +13,7 @@ p = Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
 write_api.write(bucket=bucket, record=p)
 
 ## using Table structure
-tables = query_api.query('from(bucket:"my-bucket") |> range(start: -10m)')
+tables = query_api.query('from(bucket:"test") |> range(start: -10m)')
 
 for table in tables:
     print(table)
@@ -22,7 +22,7 @@ for table in tables:
 
 
 ## using csv library
-csv_result = query_api.query_csv('from(bucket:"my-bucket") |> range(start: -10m)')
+csv_result = query_api.query_csv('from(bucket:"test") |> range(start: -10m)')
 val_count = 0
 for row in csv_result:
     for cell in row:
