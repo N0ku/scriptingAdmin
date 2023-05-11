332bc052cb748a12ca12dc295fcef1fada62c8e6
KillianVendewinkele <killian.vendewinkele@edu.esiee-it.fr>
2023-05-11 11:56:21 +0200

    MY SCRIPT RANDOM NO JOKE

--- script.py
+++ script.py
@@ -1,29 +1,23 @@
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
+from random import randint
 
-bucket = "test"
+token = "4tpJyVFMkkQ9mW8nFLjSStPCThvIF9ASgLpsQw8vDYEVYkttK42Z0IRZCD3X44GHiEx6aTDqMH6alHBdKhbJKw==" #Your token
+org = "DevTeam"
+url = "http://10.57.33.86:8087" # Your url
 
-client = InfluxDBClient(url="http://localhost:8086", token="746Ify-d_159R7Hm_SVNLsok-UGdd3iMqOg9nkGmS4YBaAMNPllhI24gEYeCkbRXJhLBKxlBt0B7lzuBn-qvJQ==", org="DevTeam")
+client = InfluxDBClient(url=url, token=token, org=org)
+
+bucket="test"
 
 write_api = client.write_api(write_options=SYNCHRONOUS)
-query_api = client.query_api()
-
-p = Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
-
-write_api.write(bucket=bucket, record=p)
-
-## using Table structure
-tables = query_api.query('from(bucket:"test") |> range(start: -10m)')
-
-for table in tables:
-    print(table)
-    for row in table.records:
-        print (row.values)
-
-
-## using csv library
-csv_result = query_api.query_csv('from(bucket:"test") |> range(start: -10m)')
-val_count = 0
-for row in csv_result:
-    for cell in row:
-        val_count += 1
+   
+for value in range(1000):
+  
+  point = (
+    Point("measurement1")
+    .tag("tagname1", "tagvalue"+str(randint(1, 10000)))
+    .field("field"+str(randint(1, 10000)), value)
+  )
+  write_api.write(bucket=bucket, org="DevTeam", record=point)
+  print(point)
