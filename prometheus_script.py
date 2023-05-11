258db33fd3c743a18434f6b8535ce785044a1236
KillianVendewinkele <killian.vendewinkele@edu.esiee-it.fr>
2023-05-11 20:32:16 +0200

    metrics created

--- prometheus_script.py
+++ prometheus_script.py
@@ -1,5 +1,5 @@
 # use https://github.com/prometheus/client_python#exporting-to-a-pushgateway for all pip installation
-from prometheus_client import start_http_server, Summary
+from prometheus_client import start_http_server, Summary, Gauge
 # import random
 import time
 import sys
@@ -7,12 +7,23 @@ import psutil
 
 # Create a metric to track time spent and requests made.
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
-# Decorate function with metric.
 @REQUEST_TIME.time()
 def process_request(t):
     time.sleep(t)
-    print(t)
-    print(psutil.cpu_times())
+    if(psutil.sensors_battery().percent > 30):
+        print(str(psutil.sensors_battery().percent) + "% de batterie")
+    else:
+        print(str(psutil.sensors_battery().percent) + "% de batterie. Faudrait commencer à recharger.")
+
+CPU_PERCENT = Gauge('cpu_percent', 'Percentage of the cpu active')
+CPU_PERCENT.set(psutil.cpu_percent(interval=2))
+
+MEMORY_USED = Gauge('memo_used', 'Memory used')
+MEMORY_USED.set(psutil.virtual_memory().percent) 
+
+BATTERY_LEFT = Gauge('battery_left', 'Percent of battery you have')
+BATTERY_LEFT.set(psutil.sensors_battery().percent)
+
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
@@ -20,3 +31,6 @@ if __name__ == '__main__':
     # Generate some requests.
     while True:
         process_request(int(sys.argv[1]))
+
+
+
