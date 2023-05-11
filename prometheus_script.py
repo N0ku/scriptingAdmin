e2ea90f7372fd94bea1c93b7266fd3e9c1954092
KillianVendewinkele <killian.vendewinkele@edu.esiee-it.fr>
2023-05-11 16:20:00 +0200

    thing

--- prometheus_script.py
+++ prometheus_script.py
@@ -1,7 +1,8 @@
 # use https://github.com/prometheus/client_python#exporting-to-a-pushgateway for all pip installation
 from prometheus_client import start_http_server, Summary
-import random
+# import random
 import time
+import sys
 
 # Create a metric to track time spent and requests made.
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
@@ -9,12 +10,12 @@ REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing requ
 # Decorate function with metric.
 @REQUEST_TIME.time()
 def process_request(t):
-    """A dummy function that takes some time."""
     time.sleep(t)
+    print(t)
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
     start_http_server(8000)
     # Generate some requests.
     while True:
-        process_request(random.random())
+        process_request(int(sys.argv[1]))
