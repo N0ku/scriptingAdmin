d80b1837e4dd103ac5690b82fbb654c3976d11cd
Zook-coder <Zooklenon11@gmail.com>
2023-05-12 11:43:00 +0200

    last prometheus script version

--- prometheus_script.py
+++ prometheus_script.py
@@ -1,36 +1,35 @@
-# use https://github.com/prometheus/client_python#exporting-to-a-pushgateway for all pip installation
-import prometheus_client
-# import random
+from prometheus_client import start_http_server, Summary, Gauge
 import time
 import sys
 import psutil
 
 # Create a metric to track time spent and requests made.
-REQUEST_TIME = prometheus_client.Summary(
-    'request_processing_seconds',
-    'Time spent processing request'
-)
+REQUEST_TIME = Summary('request_processing_seconds',
+                       'Time spent processing request')
 
 
 @REQUEST_TIME.time()
 def process_request(t):
     time.sleep(t)
+    print(CPU_PERCENT._value.get())
 
-    CPU_PERCENT = prometheus_client.Gauge(
-        'cpu_percent', 'Percentage of the cpu active')
-    CPU_PERCENT.set(psutil.cpu_percent(interval=2))
 
-    MEMORY_USED = prometheus_client.Gauge('memo_used', 'Memory used')
-    MEMORY_USED.set(psutil.virtual_memory().percent)
+CPU_PERCENT = Gauge('cpu_percent', 'Percentage of the cpu active')
 
-    BATTERY_LEFT = prometheus_client.Gauge(
-        'battery_left', 'Percent of battery you have')
-    BATTERY_LEFT.set(psutil.sensors_battery().percent)
+MEMORY_USED = Gauge('memo_used', 'Memory used')
 
+BATTERY_LEFT = Gauge('battery_left', 'Percent of battery you have')
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
-    prometheus_client.start_http_server(8000)
+    start_http_server(8000)
     # Generate some requests.
+    f = open("prometheus_data.txt", "w+")
+    f.write("CPU usage in percent Memory used in percent Battery left in percent \r")
     while True:
         process_request(int(sys.argv[1]))
+        CPU_PERCENT.set(psutil.cpu_percent())
+        MEMORY_USED.set(psutil.virtual_memory().percent)
+        BATTERY_LEFT.set(psutil.sensors_battery().percent)
+        f.write(str(CPU_PERCENT._value.get())+"%                 "+str(MEMORY_USED._value.get()
+                                                                       )+"%                  "+str(BATTERY_LEFT._value.get())+"%  \r")
