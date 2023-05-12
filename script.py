3c67e3c7758796d5809da88697380a924d81b4a4
Ruiseki <jojobru60@gmail.com>
2023-05-12 10:58:19 +0200

    major bug fix + new mesures

--- script.py
+++ script.py
@@ -1,37 +1,55 @@
-import os
-import influxdb_client
+import influxdb_client, platform, time, socket, psutil
+
+from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-BUCKET = "test"
-ORG = "DevTeam"
-token = os.getenv("INFLUX_TOKEN")
-# Store the URL of your InfluxDB instance
-url="https://us-east-1-1.aws.cloud2.influxdata.com"
+if(platform.system() != "Darwin"):
+  import clr
+  clr.AddReference(r'OpenHardwareMonitor/OpenHardwareMonitorLib')
+  from OpenHardwareMonitor.Hardware import Computer # import the .dll ( DONT WORK ON MAC )
 
-client = influxdb_client.InfluxDBClient(
-   url=url,
-   token=token,
-   org=ORG
-)
+def updateTemperature():
+  for a in range(0, len(c.Hardware[0].Sensors)):
+      if "/temperature" in str(c.Hardware[0].Sensors[a].Identifier):
+          cpuTemp = c.Hardware[0].Sensors[a].get_Value()
+          c.Hardware[0].Update()
 
-write_api = client.write_api(write_options=SYNCHRONOUS)
+  for a in range(0, len(c.Hardware[1].Sensors)):
+    if "/temperature" in str(c.Hardware[1].Sensors[a].Identifier):
+        gpuTemp = c.Hardware[1].Sensors[a].get_Value()
+        c.Hardware[1].Update()
+    return [cpuTemp, gpuTemp]
 
-p = influxdb_client.Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
-write_api.write(bucket=BUCKET, org=ORG, record=p)
+def sendData(pointName, tagName, fieldName, fieldValue):
+  point = (
+    Point(pointName)
+    .tag(tagName, socket.gethostname())
+    .field(fieldName, int(fieldValue))
+  )
+  write_api.write(bucket=buckeţ, org=org, record=point)
 
-query_api = client.query_api()
 
-query = 'from(bucket:"my-bucket")\
-|> range(start: -10m)\
-|> filter(fn:(r) => r._measurement == "my_measurement")\
-|> filter(fn:(r) => r.location == "Prague")\
-|> filter(fn:(r) => r._field == "temperature")'
+c = Computer()
+c.CPUEnabled = True
+c.GPUEnabled = True
+c.Open()
 
-result = query_api.query(org=ORG, query=query)
+token = "k3vbvIGPdyGJa1q3baR5ygN9KCK_T0km5plhDLWIFWpmxoY_WnV79X-l63WBe-EDn7PkNG63sCPLlkhAH78G7w=="
+org = "UwU"
+url = "http://192.168.165.39:8086"
 
-results = []
-for table in result:
-  for record in table.records:
-    results.append((record.get_field(), record.get_value()))
+write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+buckeţ = "Test"
+write_api = write_client.write_api(write_options=SYNCHRONOUS)
 
-print(results)
+while True:
+  if(platform.system() != "Darwin"):
+    mesure = updateTemperature()
+    if(mesure[0] != None):
+      sendData("Computer", "CPU", "Temperature", mesure[0])
+    sendData("Computer", "GPU", "Temperature", mesure[1])
+
+  sendData("Computer", "Memory", "Usage", psutil.virtual_memory().percent)
+  sendData("Computer", "CPU", "Usage", psutil.cpu_freq().current)
+  sendData("Computer", "Disk", "Usage", psutil.disk_usage('/').percent)
+  time.sleep(1)
